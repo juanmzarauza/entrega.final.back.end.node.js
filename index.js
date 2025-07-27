6d27c653d98b97a47fbda8cd77cf2f9bad0c73ec
juanmzarauza <lic.jmzarauza@gmail.com>
2025-07-26 22:49:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+app.get("/",(req,res) => {
+    res.json({message:"bienvenidos a la API REST"}
+    )
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
